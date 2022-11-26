801d3c4496a3533e3c4c35a5c5d24c377ddb0c78
Eason Lau <easonlaulearningios@gmail.com>
2022-11-26 21:06:54 +0800

    react-redux-action, useDispatch

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,23 @@ import TodoListPage from './components/TodoListPage.jsx';
 import CurrentTask from './components/CurrentTask.jsx';
 import TodoList from './components/TodoList.jsx';
 import TodoListContext from './components/TodoListContext.jsx';
-import {Provider, useSelector} from 'react-redux';
+import {Provider, useSelector, useDispatch} from 'react-redux';
 import store from './store';
+import {addTodo} from './action/todolist';
 
 const Main = ()=>{
     const todoList = useSelector(state => state.todoList);
     // const [todoList] = useState(['first','second']);
+    const [newTodo, setNewTodo] = useState('');
+    const dispatch = useDispatch();
     return (
         // <TodoListContext.Provider value={todoList}>
             <div>
                 <span>Number of todoList: {todoList.length}</span>
+                <div>
+                    <input value={newTodo} onChange={(e)=>{setNewTodo(e.target.value);}} />
+                    <button type="button" onClick={()=>{dispatch(addTodo(newTodo));}}>New item</button>
+                </div>
                 <TodoListPage />
                 <CurrentTask />
             </div>
--- src/reducer/todolist.js
+++ src/reducer/todolist.js
@@ -1,9 +1,24 @@
+import * as actions from '../action/todolist';
+
 const initState = {
     todoList: ['firstThing','secondThing'],
 }
 
 const todoReducer = (state = initState, action)=>{
     switch(action.type){
+        case actions.ADD_TODO:
+            //todoList address have no change
+            //state.todoList.push(action.payload.todo);
+            //return state;
+            //todoList have new address, trigger component to render
+            return {
+                ...state,
+                todoList:[
+                    ...state.todoList,
+                    action.payload.todo,
+                ],
+            };
+
         default:
             return state;
     }
